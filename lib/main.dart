f0083070bc419d63c872b81633b0b1d07f1c32ad
carlosquirarte27 <carlos.quirarte27@gmail.com>
2022-11-05 23:50:31 -0600

    Return main page to login page

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
-      home: Order(),
+      home: LoginPage(),
     );
   }
 }
