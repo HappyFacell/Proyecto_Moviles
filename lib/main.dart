34843b14a6680ad38c063c951aa2be7a3383264e
HappyFacell <is722820@iteso.mx>
2022-11-22 17:15:08 -0600

    Conexion con google

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: 'Material App',
-      home: SignUpPage(),
+      home: HomePage(),
     );
   }
 }
