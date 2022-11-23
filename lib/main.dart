8ea3254569e58bfdc4c86a5eea257d55885e3c98
HappyFacell <is722820@iteso.mx>
2022-11-22 18:21:37 -0600

    Correccion de errores

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       title: 'Material App',
-      home: SignUpPage(),
+      home: HomePage(),
     );
   }
 }
